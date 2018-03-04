d88ed5557c8ea6afdc657d53cbd48aecbf4d8293
Eduardo DuQuesne <eduquesne@gmail.com>
2018-03-04 17:27:18 -0600

    Update meats.js

--- javascripts/meats.js
+++ javascripts/meats.js
@@ -1,8 +1,5 @@
 /*jshint esversion: 6 */
 
-// if you cannot answer the questions in the example, do not copy and paste it. Know what's going on before you use code you didn't write
-
-// Private variable to store the different meat prices. How is it private?
 let meatPrices = { 
     ham: 1,
     turkey: 1, 
@@ -11,15 +8,9 @@ let meatPrices = {
     bacon: 1.50
 }; 
 
-//why is one key in quotes but the other isn't?
-
 const addMeat = function(meatSelection) {
     return meatPrices[meatSelection];
-  
-    //code that grabs the correct meat price and returns it
 };
-
-
 
 module.exports = { addMeat }; //What exactly is being exported here?
 
